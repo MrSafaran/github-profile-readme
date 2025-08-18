9fc14a780cc55494d68962f9d43d40529840a323
Mohammadreza <mrsafaran1380@gmail.com>
2025-08-18 03:03:44 -0700

    Update app.py and Add themes directory

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
 
 st.title(":gear: GitHub Profile Generator :gear:")
 
@@ -23,4 +26,66 @@ with st.expander("Social Media"):
     Twitter = col2.text_input("Twitter")
     Github = col1.text_input("Github")
     Youtube = col2.text_input("Youtube")
-    
+
+# Get Skills
+st.header("Technical Skills")
+with st.expander("Technical Skills"):
+    skill = st.text_input("Enter your skill:")
+
+    if st.button("Add"):
+        if skill :
+            st.session_state.skills.append(skill)
+            st.success(f"The skill '{skill}' added successfully!")
+        else:
+            st.error("Please enter a skill.")
+
+if 'skills' not in st.session_state:
+    st.session_state.skills = []
+
+st.subheader("Your Skills:")
+if 'skills' in st.session_state:
+    skills = st.session_state.skills
+    cols = st.columns(3) 
+    for i, skill in enumerate(skills):
+        cols[i % 3].write(f"- {skill}")
+
+# Select theme
+st.header("Theme")
+themes = Path("src/themes").iterdir()
+themes = [theme.name for theme in themes]
+theme = st.selectbox("Select a Theme",themes)
+st.write(f"Selected theme : **{theme}**")
+
+# Generate the readme
+if st.button("Generate README"):
+    theme_path = Path("src/themes") / theme
+    env = Environment(loader=FileSystemLoader(str(theme_path)))
+    template = env.get_template("template.md")
+
+    data = {
+        "name": name,
+        "phone": phone,
+        "email": email,
+        "website": website,
+        "location": location,
+        "Linkedin": Linkedin,
+        "Telegram": Telegram,
+        "Instagram": Instagram,
+        "Twitter": Twitter,
+        "Github": Github,
+        "Youtube": Youtube,
+        "skills": skills
+    }
+
+    output = template.render(data)
+
+    st.markdown("### 📄 Generated README")
+    st.code(output, language="markdown")
+
+    # Download button
+    st.download_button(
+        "Download README.md",
+        output,
+        file_name="README.md",
+        mime="text/markdown"
+    )
